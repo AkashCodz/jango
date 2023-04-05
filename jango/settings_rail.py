52f51cab77ab4c7a95298552c9855fda22f61f8a
Akash Sharma <akashwin1112@gmail.com>
2023-04-05 12:50:56 +0530

    static files

--- jango/settings_rail.py
+++ jango/settings_rail.py
@@ -15,8 +15,8 @@ DATABASES = {
 'OPTIONS': {'sslmode' : 'require'}
 }
 }
-# import os
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
